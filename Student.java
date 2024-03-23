de2d5acfdd0e2068237b5715ab76bfb1118ae4df
SALMA EL GHEFYRY <160774592+Salmaelghefyry@users.noreply.github.com>
2024-03-23 15:02:59 +0100

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,21 @@
+package models;
+
+import javax.imageio.plugins.tiff.ExifParentTIFFTagSet;
+
+public class Student extends Person {
+	String parentPhoneNumber ;
+	public Student(String name, int age ,String parentPhoneNumber) {
+		super(name,age);
+		this.parentPhoneNumber=parentPhoneNumber;
+	}
+	public Student() {
+		
+	}
+	
+	public String getPrentPhoneNumber() {
+		return parentPhoneNumber;
+	}
+	public void setPrentPhoneNumber(String ParentPhoneNumber ) {
+		this.parentPhoneNumber=parentPhoneNumber;
+	}
+}
